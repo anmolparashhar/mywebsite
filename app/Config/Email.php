8d42b762b146b9a8b77594df0770f3d5063c8bd5
Anmol Prashar <78647755+anmolparashhar@users.noreply.github.com>
2023-07-08 10:02:33 +0530

    Update Email.php

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -33,12 +33,12 @@ class Email extends BaseConfig
     /**
      * SMTP Username
      */
-    public string $SMTPUser = 'k95mahesh@gmail.com';
+    public string $SMTPUser = '';
 
     /**
      * SMTP Password
      */
-    public string $SMTPPass = 'mmucrglougitowpx';
+    public string $SMTPPass = '';
 
     /**
      * SMTP Port
